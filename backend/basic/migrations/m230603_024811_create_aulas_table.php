0002dbab74fb8986df892c5d493f8aeca88648c9
alejandroTelmo <alejandrodanieltelmo>
2023-06-02 23:49:28 -0300

    migracion aulas

---
+++ backend/basic/migrations/m230603_024811_create_aulas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%aulas}}`.
+ */
+class m230603_024811_create_aulas_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%aulas}}', [
+            'id' => $this->primaryKey(),
+            'descripcion' => $this->string(),
+            'ubicacion' => $this->string(50),
+            'cant_pcs' => $this->integer(),
+            'cant_proyectores' => $this->integer(),
+            'es_climatizada' => $this->boolean(),
+            'aforo' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%aulas}}');
+    }
+}
