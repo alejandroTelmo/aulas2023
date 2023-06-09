13faa48c1659162fc9a5f07fddfee121d5a9ddfe
Nehuen Merillan <nehuenmery@gmail.com>
2023-06-09 17:36:31 -0300

    Se agregó el Listado de Carreras junto a su abm. Se modifico una rule del v-slot.

---
+++ frontend/src/components/AbmCarreras.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-form v-model="valid" @submit.prevent>
+    <v-card>
+      <v-card-title>Agregar Carrera</v-card-title>
+      <v-card-text>
+        <v-text-field
+          @keyup.enter="submit"
+          v-model="nuevoNombre"
+          label="Nombre"
+          :rules="nombreRules"
+          required
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="submit" :disabled="!valid">Guardar</v-btn>
+        <v-btn color="secondary" @click="cancelar">Cancelar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-form>
+</template>
+
+<script>
+/* import { required } from "vuelidate/lib/validators"; */
+export default {
+  props: {
+    carrera: {
+      type: Object,
+      default: () => ({ id: "", nombre: "" }),
+    },
+    editar: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  /*   validations: {
+    nuevoNombre: { required },
+  }, */
+  data() {
+    return {
+      valid: false,
+      nuevoNombre: "",
+      carreraLocal: null,
+      nombreRules: [
+        v => !!v || 'El nombre es requerido',
+      ],
+    };
+  },
+  watch: {
+    carrera: {
+      handler(nuevoValor) {
+        this.nuevoNombre = nuevoValor.nombre;
+      },
+    },
+  },
+  methods: {
+    submit() {
+      console.log(this.editar);
+      if (this.editar) {
+        this.editarCarrera();
+      } else {
+        this.guardarCarrera();
+      }
+    },
+    guardarCarrera() {
+      const data = {
+        nombre: this.nuevoNombre,
+      };
+      console.log(data);
+      var that = this;
+      this.axios
+        .post("/apiv1/carrera", data)
+        .then(function (response) {
+          console.log(response);
+          that.nuevoNombre = "";
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function () {
+          that.$emit("guardar");
+        });
+    },
+    editarCarrera() {
+      const data = {
+        nombre: this.nuevoNombre,
+      };
+      var that = this;
+      this.axios
+        .patch(`/apiv1/carrera/${this.carrera.id}`, data)
+        .then(function (response) {
+          console.log(response);
+          /* alert("Registro Guardado!!"); */
+          that.nuevoNombre = "";
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function () {
+          that.$emit("guardar");
+        });
+    },
+    cancelar() {
+      this.$emit("cancelar");
+    },
+  },
+  created() {
+    this.carreraLocal = this.carrera;
+    console.log(this.carreraLocal.id, this.carreraLocal.nombre);
+    this.nuevoNombre = this.carreraLocal.nombre;
+  },
+  /*   beforeDestroy() {
+    this.nuevoNombre = "";
+  }, */
+};
+</script>
---
+++ frontend/src/components/ListadoCarreras.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="listadoCarreras"
+      :options.sync="options"
+      :server-items-length="cantTotalListadoCarreras"
+      :loading="loading"
+      :footer-props="footerProps"
+      sort-by="id"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Listado Carreras</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="agregarCarrera">Agregar Carrera</v-btn>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editarCarrera(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="eliminarCarrera(item)"> mdi-delete </v-icon>
+      </template>
+    </v-data-table>
+
+    <v-dialog v-model="mostrarAbmCarreras" max-width="500px">
+      <AbmCarreras
+        :carrera="carreraSeleccionada"
+        :editar="editar"
+        @guardar="guardarAbmCarrera"
+        @cancelar="cancelarAbmCarrera"
+      />
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import AbmCarreras from "./AbmCarreras.vue";
+export default {
+  components: {
+    AbmCarreras,
+  },
+  data() {
+    return {
+      cantTotalListadoCarreras: 0,
+      loading: true,
+      options: {},
+      headers: [
+        { text: "ID", value: "id" },
+        { text: "Nombre", value: "nombre" },
+        { text: "Acciones", value: "actions", sortable: false },
+      ],
+      footerProps:{
+        'items-per-page-options': [10, 25, 50]
+      },
+      listadoCarreras: [],
+      mostrarAbmCarreras: false,
+      carreraSeleccionada: {},
+      editar: false,
+    };
+  },
+  watch: {
+    options: {
+      handler() {
+        this.obtenerListadoDeApi();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    obtenerListadoDeApi() {
+      const { sortBy, sortDesc, page, itemsPerPage } = this.options;
+      let sortMode = "";
+      if (sortDesc[0]) {
+        sortMode = "-";
+      }
+      console.log(sortDesc, sortMode);
+      var that = this;
+      this.axios
+        .get(
+          `/apiv1/carrera?per-page=${itemsPerPage}&page=${page}&sort=${sortMode}${sortBy}`
+        )
+        .then(function (response) {
+          if (response.status === 200) {            
+            that.listadoCarreras = response.data;
+            that.cantTotalListadoCarreras = parseInt(
+              response.headers["x-pagination-total-count"]
+            );
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function () {
+          that.loading = false;
+        });
+    },
+    mostrarFormularioAbmCarreras() {
+      this.mostrarAbmCarreras = true;
+    },
+    agregarCarrera() {
+      this.editar = false;
+      this.carreraSeleccionada = {};
+      this.mostrarFormularioAbmCarreras();
+    },
+    editarCarrera(carrera) {
+      console.log(carrera.nombre);
+      this.carreraSeleccionada = {
+        id: carrera.id,
+        nombre: carrera.nombre,
+      };
+      this.editar = true;
+      this.mostrarAbmCarreras = true;
+    },
+    guardarAbmCarrera() {
+      console.log("Carrera Guardada con exito!");
+      this.mostrarAbmCarreras = false;
+      this.obtenerListadoDeApi();
+    },
+    eliminarCarrera(carrera) {
+      var that = this;
+      this.axios
+        .delete(`/apiv1/carrera/${carrera.id}`)
+        .then(function (response) {
+          console.log(response);
+          alert("Registro Eliminado!!");
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function () {
+          that.obtenerListadoDeApi();
+        });
+    },
+    cancelarAbmCarrera() {
+      this.mostrarAbmCarreras = false;
+      this.carreraSeleccionada.nombre = "";
+      this.carreraSeleccionada.id = "";
+    },
+  },
+};
+</script>
