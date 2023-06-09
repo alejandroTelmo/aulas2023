1240efb79a0f50684157c76f4be028a00f57abb6
Nehuen Merillan <nehuenmery@gmail.com>
2023-06-09 17:31:23 -0300

    Creacion de un baseController: Para permitir peticiones desde el frontend y configurar los metodos aceptados junto con otros parametros.

---
+++ backend/basic/modules/apiv1/controllers/BaseController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\modules\apiv1\controllers;
+
+use yii\rest\ActiveController;
+use yii\filters\Cors;
+
+
+class BaseController extends ActiveController
+{
+    public static function allowedDomains()
+    {
+        return [$_SERVER["REMOTE_ADDR"], 'http://127.0.0.1:8080']; //todo: Agregar la url como variable en el .env
+    }
+
+    function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        return array_merge($behaviors, [
+            'corsFilter' => [
+                'class' => Cors::class,
+                'cors' => [
+                    'Origin' => static::allowedDomains(),
+                    'Access-Control-Request-Method' => ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age' => 3600,
+                    'Access-Control-Allow-Headers' => ['authorization', 'X-Requested-With', 'content-type'],
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page', 'X-Pagination-Page-Count', 'X-Pagination-Total-Count']
+                ]
+            ]
+        ]);
+    }
+}
