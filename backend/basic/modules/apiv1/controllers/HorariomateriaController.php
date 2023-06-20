8c5aa3a849c15f88b3708294e79dda29aa3666ed
Nehuen Merillan <nehuenmery@gmail.com>
2023-06-20 01:39:23 -0300

    Se reescribio el action index de HorariomateriaController: Se reescribio el action index para que incluya en la data la materia, para usarla en el listado. Se agrego a HorarioMateriaQuery metodos para paginar la respuesta.

--- backend/basic/modules/apiv1/controllers/HorariomateriaController.php
+++ backend/basic/modules/apiv1/controllers/HorariomateriaController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\apiv1\controllers;
 
+use Yii;
 
 /**
  * Default controller for the `apiv1` module
@@ -9,4 +10,34 @@ namespace app\modules\apiv1\controllers;
 class HorariomateriaController extends BaseController
 {
     public $modelClass = 'app\modules\apiv1\models\HorarioMateria';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        $actions['index'] = [
+            'class' => 'yii\rest\IndexAction',
+            'modelClass' => $this->modelClass,
+            'checkAccess' => [$this, 'checkAccess'],
+            'prepareDataProvider' => function ($action) {
+
+                $requestParams = Yii::$app->getRequest()->getBodyParams();
+                if (empty($requestParams)) {
+                    $requestParams = Yii::$app->getRequest()->getQueryParams();
+                }
+
+                $dataProvider = (new $this->modelClass)->find()
+                    ->getWithMateria()
+                    ->paginate($requestParams['page'], $requestParams['per-page'])
+                    ->orderBy($requestParams['sort']);
+
+                Yii::$app->response->headers->set('X-Pagination-Current-Page', $dataProvider->getPage());
+                Yii::$app->response->headers->set('X-Pagination-Page-Count', $dataProvider->getPerPage());
+                Yii::$app->response->headers->set('X-Pagination-Total-Count', $dataProvider->getTotalCount());
+
+                return $dataProvider->asArray()->all();
+            }
+        ];
+        return $actions;
+    }
 }
