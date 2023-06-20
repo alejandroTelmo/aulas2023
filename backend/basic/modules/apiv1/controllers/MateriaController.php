3f9c4b2e2b29e6d93f0d15a03ac7640269931cee
Nehuen Merillan <nehuenmery@gmail.com>
2023-06-20 01:43:00 -0300

    Se agrego a MateriaController un action buscar: Se agrego un action buscar que recibe en el queryParams parte del nombre y realiza una busqueda en la base con "ilike".

--- backend/basic/modules/apiv1/controllers/MateriaController.php
+++ backend/basic/modules/apiv1/controllers/MateriaController.php
@@ -9,4 +9,24 @@ namespace app\modules\apiv1\controllers;
 class MateriaController extends BaseController
 {
     public $modelClass = 'app\modules\apiv1\models\Materia';
+
+    
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        $actions['buscar'] = [
+            'class' => 'yii\rest\IndexAction',
+            'modelClass' => $this->modelClass,
+            'checkAccess' => [$this, 'checkAccess'],
+            'prepareDataProvider' => function ($action) {
+                $materiaModel = new \app\modules\apiv1\models\Materia();
+                $dataProvider = $materiaModel->buscar(\Yii::$app->request->queryParams);
+
+                return $dataProvider;
+            }
+        ];
+
+        return $actions;
+    }
 }
--- backend/basic/modules/apiv1/models/Materia.php
+++ backend/basic/modules/apiv1/models/Materia.php
@@ -2,7 +2,21 @@
 
 namespace app\modules\apiv1\models;
 
+use yii\data\ActiveDataProvider;
+
 class Materia extends \app\models\Materia
 {
     
+    public function buscar($params)
+    {
+        $query = Materia::find();
+
+        $query->andFilterWhere(['ilike', 'nombre', $params['nombre']]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $dataProvider;
+    }
 }
