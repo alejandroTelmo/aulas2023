8c26656251ff1112120496741dc08e424f7a1d9b
Nehuen Merillan <nehuenmery@gmail.com>
2023-06-23 13:27:27 -0300

    Se modifico el action create de HorarioMateriaController: Se sobreescribio el action create para recibir un lote de datos y guardarlo en la base con una transaccion.

--- backend/basic/modules/apiv1/controllers/HorariomateriaController.php
+++ backend/basic/modules/apiv1/controllers/HorariomateriaController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\apiv1\controllers;
 
+use app\modules\apiv1\models\HorarioMateria;
 use Yii;
 
 /**
@@ -15,7 +16,7 @@ class HorariomateriaController extends BaseController
     {
         $actions = parent::actions();
 
-        $actions['index'] = [
+        /*         $actions['index'] = [
             'class' => 'yii\rest\IndexAction',
             'modelClass' => $this->modelClass,
             'checkAccess' => [$this, 'checkAccess'],
@@ -37,7 +38,52 @@ class HorariomateriaController extends BaseController
 
                 return $dataProvider->asArray()->all();
             }
-        ];
+        ]; */
+        unset($actions['index']);
+        unset($actions['create']);
         return $actions;
+    }
+
+    public function actionCreate()
+    {
+        $requestParams = Yii::$app->getRequest()->getBodyParams();
+        $transaction = HorarioMateria::getDb()->beginTransaction();
+        try {
+            foreach ($requestParams as $value) {
+                $horarioMateria = new HorarioMateria();
+                $horarioMateria->attributes = $value;
+                $horarioMateria->save();
+            }
+            $transaction->commit();
+            return $this->asJson([
+                'success' => true,
+                'data' => $requestParams,
+            ]);
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        } catch (\Throwable $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+    }
+
+    public function actionIndex()
+    {
+        $requestParams = Yii::$app->getRequest()->getBodyParams();
+        if (empty($requestParams)) {
+            $requestParams = Yii::$app->getRequest()->getQueryParams();
+        }
+
+        $dataProvider = (new $this->modelClass)->find()
+            ->getWithMateria()
+            ->paginate($requestParams['page'], $requestParams['per-page'])
+            ->orderBy($requestParams['sort']);
+
+        Yii::$app->response->headers->set('X-Pagination-Current-Page', $dataProvider->getPage());
+        Yii::$app->response->headers->set('X-Pagination-Page-Count', $dataProvider->getPerPage());
+        Yii::$app->response->headers->set('X-Pagination-Total-Count', $dataProvider->getTotalCount());
+
+        return $dataProvider->asArray()->all();
     }
 }
