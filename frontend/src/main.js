b93f5cfd62aaf29f4bb2733a9af1b95771310519
Federico.M <federicopili@hotmail.com>
2023-06-30 18:09:37 -0300

    Creacion del store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import VueAxios from 'vue-axios'
-import custom_axios from './plugins/axios'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueAxios from "vue-axios";
+import custom_axios from "./plugins/axios";
+import router from "./router";
+import store from "./plugins/store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueAxios, custom_axios)
+Vue.use(VueAxios, custom_axios);
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
