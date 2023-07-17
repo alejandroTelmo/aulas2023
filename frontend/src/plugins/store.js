0abeaedbb718667766812ea66aee46be95af94ff
Nehuen Merillan <nehuenmery@gmail.com>
2023-07-17 14:13:55 -0300

    Agregando al store la informacion del usuario.

--- frontend/src/plugins/store.js
+++ frontend/src/plugins/store.js
@@ -7,30 +7,42 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     loggedIn: false,
+    userInfo: {
+      'username': '',
+      'accessToken': '',
+      'nombre': '',
+      'apellido': '',
+    }
   },
   mutations: {
-    successfulLogin(state,loggedIn){
-      state.loggedIn = loggedIn;
-      console.log(state.loggedIn,)
+    successfulLogin(state,userInfo){
+      state.loggedIn = true
+      state.userInfo = userInfo
       router.push('/home')
     },
     logout(state,loggedIn){
-      state.loggedIn = loggedIn;
-      console.log(state.loggedIn,)
+      state.loggedIn = loggedIn
+      state.userInfo.username = ''
+      state.userInfo.accessToken = ''
+      state.userInfo.nombre = ''
+      state.userInfo.apellido = ''
+      console.log(state.loggedIn)
       router.push('/')
     }
   },
   actions: {
-    loginAttempt({commit}){
+    loginAttempt({commit}, userInfo){
       console.log("login")
-      commit('successfulLogin',true)
+      commit('successfulLogin', userInfo)
     },
     logoutAttempt({commit}){
       commit('logout',false)
     }
   },
   getters: {
-  
+    token: state => {
+      return state.userInfo.accessToken;
+    }
   }
 });
 
