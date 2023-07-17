b86ff1b29c6bc6ad83a3eccf853bbf213dbb6f81
Nehuen Merillan <nehuenmery@gmail.com>
2023-07-17 14:14:23 -0300

    Agregando request interceptor con el uso del token del usuario.

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import store from "@/plugins/store";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+const custom_axios = axios.create(
+    {
+        baseURL: 'http://127.0.0.1:8000',
+    }
+)
 
-const custom_axios = axios;
+custom_axios.interceptors.request.use(
+    function(config){
+        const token = store.getters.token;
+        if (token.length > 0) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config;
+    }
+)
 
 export default custom_axios;
